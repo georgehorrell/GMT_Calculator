a1e0173c24d0d0683c73d4e27577faefc7c6f499
georgehorrell <georgewphorrell@gmail.com>
2014-12-09 18:53:32 +0000

    changed icon removed watermark changed button style

--- app/src/main/java/com/georgehorrell/gmtcalculator/selectBoatClass.java
+++ app/src/main/java/com/georgehorrell/gmtcalculator/selectBoatClass.java
@@ -168,17 +168,7 @@ public class selectBoatClass extends Activity implements TextWatcher {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+
 
     public void setGMTview(View view, int time) {
         // Function converts raw seconds into timeString
